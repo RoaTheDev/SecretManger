810a5aa9262a0da662b2b2200321550b4cc47913
RoaTheDev <roachamreun15@proton.me>
2026-03-13 22:55:04 +0700

    init proj

---
+++ src/main/java/io/roa/secretmanger/SecretMangerAditiFinalApplication.java
@@ -0,0 +1,13 @@
+package io.roa.secretmanger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecretMangerAditiFinalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecretMangerAditiFinalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/roa/secretmanger/SecretMangerAditiFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package io.roa.secretmanger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecretMangerAditiFinalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
